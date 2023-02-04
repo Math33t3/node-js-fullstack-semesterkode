045a98652b36a1c8d157d2b3e3f2c8e631f70594
Math33t3 <math33t3@stud.kea.dk>
2023-02-04 12:57:22 +0100

    solved the 3.objects and 4.arrays

---
+++ 00._Miscellaneuos/1._Intro_Exercises/3.Objects.js
@@ -0,0 +1,36 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObj = { message: "Hello, earthling! I bring peace." };
+console.log(myObj.message);
+// Log the message 
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+const me = { name: "Mathias", age : 29 };
+console.log(me);
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = {};
+stackOverflow.isAllowed = true;
+console.log(stackOverflow.isAllowed);
+// make a rule called isAllowed and let the value be true
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = { description: "The best song in the world." };
+delete thisSong.description;
+thisSong.about = "Just a tribute";
+console.log(thisSong);
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+
+// --------------------------------------
+
+
