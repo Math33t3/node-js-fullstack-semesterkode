37530ef15e0fa17fed67fae1682b23510323c53b
Math33t3 <math33t3@stud.kea.dk>
2023-02-15 15:03:07 +0100

    a few changes added

--- 00._Miscellaneuos/01._BirdsAPI/01._GET_methods/app.js
+++ 00._Miscellaneuos/01._BirdsAPI/01._GET_methods/app.js
@@ -6,18 +6,18 @@ const app = express();
 const birds = [{name: "Swan", id: "1"}, {name: "Peacock", id: "2"}
 ,{name: "Crow", id: "3"}, {name: "Raven", id: "4"}];
 
-
+//making our first endpoint
 app.get("/birds", (req, res) => {
     res.send(birds);
 });
 
 
 app.get( "/birds/:id", (req, res) => {
-    //Making the response and error message.
     const bird = birds.find(bird => bird.id === req.params.id);
     console.log(req.params);
+    //Making the response and error message.
     if (!bird) {
-        res.status(404).send("Bird not found.");
+        res.status(404).send("Your bird was not found.");
     } else{
         res.send(bird);
     }
